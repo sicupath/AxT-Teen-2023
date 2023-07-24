62e4a07a1cae36234b6cf91c13d1903f806b3cfe
Sofia Ingigerth Canas Urbina <84815840+SICU2001@users.noreply.github.com>
2023-07-24 11:23:55 -0600

    Create watermelon.cpp

---
+++ Sesion 1/watermelon.cpp
@@ -0,0 +1,23 @@
+// Librerías
+#include <iostream>
+
+// Libería estándar
+using namespace std;
+
+// En la función main tenemos la solución
+int main() {
+  // Crear las variables de entrada
+  int w;
+  
+  // Recibir los datos
+  cin >> w;
+
+  // Si es imapr o 2, no se puede dividir la sandía como se pide
+  if(w%2==1 || w==2){
+    cout << "NO" << endl;
+  }else{ // En caso contrario sí se puede
+    cout << "SI" << endl;
+  }
+  
+  
+}
