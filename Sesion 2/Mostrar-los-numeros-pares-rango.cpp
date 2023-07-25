6cb38113934843156f322ec49fc0cf779cbfade2
Alonso Huerta <alonso12_284@outlook.com>
2023-07-25 11:14:56 -0600

    Create Mostrar-los-numeros-pares-rango.cpp

---
+++ Sesion 2/Mostrar-los-numeros-pares-rango.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int n, m;
+  cin >> n >> m;
+
+  for(int i = n; i <= m; i++){
+    if(i%2 == 0){
+      cout << i << endl;
+    }
+  }
+
+}
